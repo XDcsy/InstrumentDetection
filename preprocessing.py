732eac94bf35ae7aed1d94f6e78da73b8f70ad79
neEverett <shengyang.cheng@outlook.com>
2017-04-05 14:52:53 +0800

    预处理基本完成

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from scipy.io import wavfile
-from numpy import array
 from scipy import signal
+from scipy.io import wavfile
+import numpy as np
 #from pylab import plot
 
 #帧的类
@@ -9,9 +9,11 @@ from scipy import signal
 #window:窗
 #start:这一帧的起始取样点
 class frame:
-    def __init__(self, audio, window, start):
-        self.start = start
-        self.data = window * audio #因为下标个数一致，直接对应相乘加窗
+	def __init__(self, audio, window, start):
+		self.start = start
+		self.data = window * audio #因为下标个数一致，直接对应相乘加窗
+		self.energy = sum(self.data**2) #计算短时能量 Reference:http://www.docin.com/p-1055762767.html
+
 
 
 #读取音频
@@ -27,7 +29,7 @@ freq, music = wavfile.read(r'e:\shutdown.wav')
 #http://blog.sina.com.cn/s/blog_e9d8e2610102uxgv.html
 #http://www.docin.com/p-318611828.html
 #http://www.mamicode.com/info-detail-502860.html
-b = array([1, -0.95])
+b = np.array([1, -0.95])
 left = signal.lfilter(b, 1, music[:,0])
 right = signal.lfilter(b, 1, music[:,1])
 #left,right:预加重后的左右声道
@@ -42,12 +44,13 @@ frameLength = int(freq/(1/frameTime)) #以取样点数表示的帧长
 frameShift = int(frameLength/2) #帧移
 hammingWindow = signal.hamming(frameLength) #生成汉明窗
 
-f = []
-for i in range(0, len(left) - frameLength, frameShift): #最后长度不足一帧的部分需舍去
-    f.append(frame(left[i:i+frameLength], hammingWindow, i))
-for i in range(0, len(right) - frameLength, frameShift): #对左右声道独立地做分帧
-    f.append(frame(right[i:i+frameLength], hammingWindow, i))
+f = [] #记录所有帧对象的列表
+for i in range(0, len(left) - frameLength, frameShift): #音频最后长度不足一帧的部分需舍去
+	f.append(frame(left[i:i+frameLength], hammingWindow, i))
+for i in range(0, len(right) - frameLength, frameShift): #对左右声道独立地分帧，在列表中右声道接在左声道后面
+	f.append(frame(right[i:i+frameLength], hammingWindow, i))
 
-
-
-
+#计算所有帧的短时能量，算出阈值
+energy = np.array([frame.energy for frame in f]) #短时能量
+Lenergy = np.amin(energy) + (np.amax(energy) - np.amin(energy))*0.03 #能量阈值：把与最低能量相距3%以内的帧认为是静音帧
+f = [frame for frame in f if frame.energy > Lenergy] #更新对象列表，只保留非静音帧
