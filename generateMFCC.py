312094d9f0ecf7c7e14919f91b3f5f39b6a6f34a
neEverett <shengyang.cheng@outlook.com>
2017-04-10 18:33:53 +0800

    添加批量读取功能

--- generateMFCC.py
+++ generateMFCC.py
@@ -1,25 +1,35 @@
 # -*- coding: utf-8 -*-
+import os
+import re
 import numpy as np
 
 from scipy.io import wavfile
 from preprocessing import preprocessing
 from mfcc import mfcc
+#获取此文件所在文件夹的名称，用于在保存结果时命名
+foldername = os.path.basename(os.getcwd())
 
-#读取音频
-#fs:采样率，录音设备在一秒钟内对声音信号的采样次数
-#music:原始音频
-fs, music = wavfile.read(r'e:\shutdown.wav')
-if music.dtype.type not in [np.int16, np.int32, np.float32]:
-    raise TypeError('only 16bit,32bit PCM and 32bit floating-point wavefiles are supported')
-#只支持这三种精度的wav文件，注意不支持8bit与24bit
+#读取当前目录下音频的文件名
+filename = [name for name in os.listdir(os.getcwd()) if re.match(r'.*wav',name)]
+for i in range(len(filename)):
+    #fs:采样率，录音设备在一秒钟内对声音信号的采样次数
+    #music:原始音频
+    fs, music = wavfile.read(filename[i])
+    if music.dtype.type not in [np.int16, np.int32, np.float32]:
+        raise TypeError('only 16bit,32bit PCM and 32bit floating-point wavefiles are supported')
+        #只支持这三种精度的wav文件，注意不支持8bit与24bit
 
-#预处理
-frameTime = 0.02 #(s)
-#f：处理后的各帧对象 frameLength：以取样点数表示的帧长
-f, frameLength = preprocessing(music, fs, frameTime)
+    #预处理
+    frameTime = 0.02 #(s)
+    #f：处理后的各帧对象 frameLength：以取样点数表示的帧长
+    f, frameLength = preprocessing(music, fs, frameTime)
 
-#计算出mfcc
-ceps = mfcc(f, fs, frameLength)
+    #计算出mfcc
+    tmpceps = mfcc(f, fs, frameLength)
+    if (i == 0):
+        ceps = tmpceps
+    else:
+        ceps = np.concatenate((ceps, tmpceps), axis=0)
 
-np.save("mfcc_results", ceps) #保存mfcc参数
-#ceps = np.load("mfcc_results") #读取mfcc参数
+np.save(foldername, ceps) #保存mfcc参数
+#ceps = np.load("name") #读取mfcc参数
