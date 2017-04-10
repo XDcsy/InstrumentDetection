c179ca2aaad2f4bf3964e72cd613e179f4d999aa
neEverett <shengyang.cheng@outlook.com>
2017-04-10 23:55:59 +0800

    BUG fix

---
+++ test.py
@@ -0,0 +1,35 @@
+import os
+import re
+import pickle
+import numpy as np
+
+from scipy.io import wavfile
+from preprocessing import preprocessing
+from mfcc import mfcc
+
+#读取模型
+clf = pickle.load(open('model_svm', 'rb'))
+#读取标签对应的乐器名称
+instruments = pickle.load(open('names', 'rb'))
+
+#读取当前目录下音频的文件名
+filename = [name for name in os.listdir(os.getcwd()) if re.match(r'.*wav',name)]
+result = []
+for i in range(len(filename)):
+    fs, music = wavfile.read(filename[i])
+    if music.dtype.type not in [np.int16, np.int32, np.float32]:
+        raise TypeError('only 16bit,32bit PCM and 32bit floating-point wavefiles are supported')
+        #只支持这三种精度的wav文件，注意不支持8bit与24bit
+
+    #预处理
+    frameTime = 0.02 #(s)
+    #f：处理后的各帧对象 frameLength：以取样点数表示的帧长
+    f, frameLength = preprocessing(music, fs, frameTime)
+
+    #计算出mfcc
+    ceps = mfcc(f, fs, frameLength)
+    result.append(clf.predict(ceps)) #得到对每一帧的预测结果
+
+for i in range(len(result)):
+    result[i] = np.argmax(np.bincount(result[i])) #每帧投票，选出得票最多的结果
+    print("The instrument played in "+filename[i]+" is detected as: "+instruments[result[i]])
--- trainmodel_SVM.py
+++ trainmodel_SVM.py
@@ -6,11 +6,18 @@ from sklearn import svm
 import numpy as np
 
 insrumentName = [name for name in os.listdir(os.getcwd()) if re.match(r'.*npy',name)]
-ceps = []
+lable = []
 for i in range(len(insrumentName)):
-    ceps.append(np.load(insrumentName[i])) #读取mfcc参数
-
+    if (i == 0):
+        ceps = np.load(insrumentName[i])  #读取mfcc参数
+        lable.extend([i]*ceps.shape[0])  #生成标签
+    else:
+        tmpceps = np.load(insrumentName[i])
+        ceps = np.concatenate((ceps, tmpceps), axis=0)
+        lable.extend([i]*tmpceps.shape[0])
+        
 clf = svm.SVC(decision_function_shape='ovo')
-clf.fit(ceps, insrumentName)
+clf.fit(ceps, lable)
 pickle.dump(clf,open('model_svm','wb'))
+pickle.dump(insrumentName,open('names','wb'))
 # getback = pickle.load(open('model_svm', 'rb'))
